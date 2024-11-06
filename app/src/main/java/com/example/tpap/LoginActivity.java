671b7ce1e9053374e882019fabd222a5a481f1e9
강형석 <fluor.kang@gmail.com>
2024-11-06 21:22:05 +0900

    feat: initial implementation of LoginActivity and login.xml

--- app/src/main/java/com/example/tpap/LoginActivity.java
+++ app/src/main/java/com/example/tpap/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.tpap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    Button login_button;
+    EditText username_editText;
+    EditText password_editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        login_button = findViewById(R.id.login_button);
+        username_editText = findViewById(R.id.username_editText);
+        password_editText = findViewById(R.id.password_editText);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = username_editText.getText().toString();
+                String password = password_editText.getText().toString();
+                if (username.equals("qwer") && password.equals("qwer"))
+                {
+                    Log.i("Transition", "LoginActivity -> MainActivity");
+                }
+            }
         });
     }
 }
