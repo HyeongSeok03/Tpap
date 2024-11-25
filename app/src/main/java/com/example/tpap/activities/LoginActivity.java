a7a4b6ec9af77e273d1a85ca943b87fed6244916
강형석 <fluor.kang@gmail.com>
2024-11-26 00:49:52 +0900

    feat: Add visibility toggle for password field in LoginActivity
    - Implemented visibility toggle button for password EditText in LoginActivity.
    - Added StateListDrawable using baseline_visibility_24.xml and baseline_visibility_off_24.xml.
    - Created password_visibility.xml for managing button states.
    - Modified activity_login.xml to include the visibility button next to the password field.
    - Updated LoginActivity to handle inputType changes and toggle button states.

--- app/src/main/java/com/example/tpap/activities/LoginActivity.java
+++ app/src/main/java/com/example/tpap/activities/LoginActivity.java
@@ -2,10 +2,12 @@ package com.example.tpap.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     Button login_button;
     EditText username_editText;
     EditText password_editText;
+    ImageButton visibilityToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
         login_button = findViewById(R.id.login_button);
         username_editText = findViewById(R.id.username_editText);
         password_editText = findViewById(R.id.password_editText);
+        visibilityToggle = findViewById(R.id.visibility_toggle);
 
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +40,23 @@ public class LoginActivity extends AppCompatActivity {
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
+            }
+        });
+
+        visibilityToggle.setOnClickListener( v -> {
+            int type = password_editText.getInputType();
+            Log.e("check", String.valueOf(username_editText.getInputType()));
+            if (type == 129)
+            {
+                password_editText.setInputType(1);
+                password_editText.setSelection(password_editText.getText().length()); // 커서 유지
+                visibilityToggle.setSelected(true);
+            }
+            else
+            {
+                password_editText.setInputType(129);
+                password_editText.setSelection(password_editText.getText().length()); // 커서 유지
+                visibilityToggle.setSelected(false);
             }
         });
     }
